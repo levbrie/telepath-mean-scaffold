96728dbfa83c9f8e8baa4ae06fbae4a3139d4df9
Lev Brie <lev.brie@gmail.com>
2014-07-28 14:34:53 -0400

    loading in files to watch from a json file, using require instead of readJSON, and creating a config object to then use with the call to grunt.initConfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,20 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+  var config = {
+    pkg: require('./package.json'),
+    env: process.env,
+    files: grunt.file.readJSON('./grunt/files.json'),
     config: grunt.file.readJSON('./grunt/config.json'),
     watch: {
       js: {
-        files: [
-          'public/app/{,*//}*.js',
-          'public/app/*/*/*.js',
-          'server.js'
-        ],
+        files: ["<%= files.js.public %>", "<%= files.js.server %>"],
         tasks: ['default']
       }
     }
-  });
+  };
+  grunt.initConfig(config);
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', function() {
     grunt.log.writeln('Grunt grunt');
-    grunt.log.writeln('Grunt Author: ' + grunt.config.get('config.author').firstName);
+    grunt.log.writeln('Grunt Author <%= pkg.engines.node %>: ' + grunt.config.get('config.author').firstName);
   });
 };
