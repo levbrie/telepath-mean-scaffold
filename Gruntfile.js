bdd6688b2f9add68e28644e715c57965979b74a2
Lev Brie <lev.brie@gmail.com>
2014-07-28 13:51:53 -0400

    add grunt-contrib-watch and create first file watch, add add grunt-express-server and add grunt-wiredep (not yet setup)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,21 @@
 module.exports = function(grunt) {
-  grunt.initConfig(grunt.file.readJSON('./grunt/config.json'));
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    config: grunt.file.readJSON('./grunt/config.json'),
+    watch: {
+      js: {
+        files: [
+          'public/app/{,*//}*.js',
+          'public/app/*/*/*.js',
+          'server.js'
+        ],
+        tasks: ['default']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', function() {
     grunt.log.writeln('Grunt grunt');
-    grunt.log.writeln('Grunt Author: ' + grunt.config.get('author').firstName);
+    grunt.log.writeln('Grunt Author: ' + grunt.config.get('config.author').firstName);
   });
 };
