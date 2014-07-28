ce216d508f3981bec22e1d00ea82727037dc0546
Lev Brie <lev.brie@gmail.com>
2014-07-28 14:42:19 -0400

    add grunt-contrib-uglify along with an uglify task that builds to dist on file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,14 +7,22 @@ module.exports = function(grunt) {
     watch: {
       js: {
         files: ["<%= files.js.public %>", "<%= files.js.server %>"],
-        tasks: ['default']
+        tasks: ['default', 'uglify']
+      }
+    },
+    uglify: {
+      dist:{
+        files:{
+          "dist/app.min.js":["<%= files.js.public %>"]
+        }
       }
     }
   };
   grunt.initConfig(config);
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', function() {
     grunt.log.writeln('Grunt grunt');
-    grunt.log.writeln('Grunt Author <%= pkg.engines.node %>: ' + grunt.config.get('config.author').firstName);
+    grunt.log.writeln('Grunt Author: ' + grunt.config.get('pkg.author'));
   });
 };
